353de777112149d26e841b38705737d5d4749aac
Osca <Oscakholopha@gmail.com>
2025-09-04 11:29:07 +0200

    Layout of the entire Project

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 // script.js
-const navItems = document.querySelectorAll('.nav li');
-
-navItems.forEach(item => {
-  item.addEventListener('click', () => {
-    navItems.forEach(i => i.classList.remove('active'));
-    item.classList.add('active');
-  });
-});
